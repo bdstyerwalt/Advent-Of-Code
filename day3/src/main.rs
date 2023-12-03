e7e56d82e0a93bd1330cdd8f4c2d1c1e58e4ed2d
bdstyerwalt <bdstyerwalt@gmail.com>
2023-12-03 11:14:45 -0800

    Completed Day 3 Part 1

---
+++ day3/src/main.rs
@@ -0,0 +1,90 @@
+use std::{fs, error::Error, collections::HashSet};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_file: String = fs::read_to_string("input.txt")?;
+
+    println!("\n\n---------Day 3---------");
+    println!("Part 1: {}", &part1(&input_file));
+    //println!("Part 2: {}", &part2(&input_file));
+    println!("----------------------");
+    
+    return Ok(())
+}
+
+fn part1(input_file: &String) -> i32 {
+    
+    let symbol_locs: HashSet<(i32, i32)> = find_symbols(input_file);
+    let number_locs: Vec<(i32, i32, i32, i32)> = find_numbers(input_file);
+
+    let mut acc: i32 = 0;
+    for (val, row, left, right) in number_locs {
+        let mut border_points: HashSet<(i32, i32)> = HashSet::new();
+        let (row, left, right) = (row as i32, left as i32, right as i32);
+        //println!("----------\n{}: {}, ({},{})", val, row, left, right);
+        for y in row-1..=row+1 {
+            for x in left-1..=right+1 {
+                //println!("{},{}", y, x);
+                border_points.insert((y, x));
+            }
+        }
+        let intersect = border_points.intersection(&symbol_locs);
+        if intersect.count() > 0 {
+            acc += val;
+            //println!("INTERSECT: {}->{}", acc-val, acc);
+        }
+    }
+    return acc;
+}
+
+fn find_symbols(input_file: &String) -> HashSet<(i32, i32)> {
+    let mut symbol_locs: HashSet<(i32, i32)> = HashSet::new();
+    for (row, line) in input_file.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            if !c.is_numeric() && c != '.' {
+                //println!("{} {},{}", &c.to_string(), row, col);
+                symbol_locs.insert((row as i32, col as i32));
+            }
+        }
+    }
+    return symbol_locs;
+}
+
+fn find_numbers(input_file: &String) -> Vec<(i32, i32, i32, i32)> {
+    let mut number_locs: Vec<(i32, i32, i32, i32)> = Vec::new();
+    
+    for (row, line) in input_file.lines().enumerate() {
+        let mut active_num: bool = false;
+        let mut val: i32 = 0;
+        let mut left: i32 = 0;
+        let mut right: i32 = 0;
+        for (col, c) in line.chars().enumerate() {
+            if !active_num && !c.is_numeric() {
+                continue
+                
+            } else if !active_num && c.is_numeric() {
+                active_num = true;
+                (val, left, right) = (c.to_string().parse().unwrap(), col as i32, col as i32);
+                if col == line.len()-1 {
+                    number_locs.push((val, row as i32, left, right));
+                }
+
+            } else if active_num && c.is_numeric() {
+                (val, right) = (val*10 + c.to_string().parse::<i32>().unwrap(), col as i32);
+                //println!("END OF LINE: {}, {}", col, line.len()-1);
+                if col == line.len()-1 { // EOL condition
+                    //println!("{}: {}, ({},{})", val, row, left, right);
+                    number_locs.push((val, row as i32, left, right));
+                }
+                
+            } else if active_num && !c.is_numeric() {
+                active_num = false;
+                //println!("{}: {}, ({},{})", val, row, left, right);
+                number_locs.push((val, row as i32, left, right))
+            }
+        }
+    }
+    return number_locs;
+
+}
+
+//fn part2(input_file: &String) {  }
