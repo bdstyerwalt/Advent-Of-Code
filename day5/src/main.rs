56fc0a6dd67011a488565359498d1c998678947f
bdstyerwalt <bdstyerwalt@gmail.com>
2023-12-05 12:54:49 -0800

    Completed Day 5 Part 1 (reduced map sizes)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,15 +1,14 @@
-use std::{fs, error::Error, collections::HashMap, vec};
-use std::cmp::min;
+use std::{fs, error::Error, collections::HashMap};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input_file: String = fs::read_to_string("sample.txt")?;
+    let input_file: String = fs::read_to_string("input.txt")?;
 
     let p1_res: i64 = part1(&input_file);
 
     println!("\n\n---------Day 5---------");
     println!("Part 1: {}", p1_res);
     //println!("Part 2: {}", part2(game_cards));
-    println!("----------------------");
+    println!("-----------------------");
     
     return Ok(())
 }
@@ -22,26 +21,22 @@ fn part1(input_lines: &String) -> i64 {
     seeds = seeds.split(": ").nth(1).unwrap();
     let mut seeds: Vec<i64> = seeds.split_whitespace().map(|x| x.trim().parse().unwrap()).collect();
     
-    let mut tmp_map: HashMap<i64, i64> = HashMap::new();
+    let mut seed_map: HashMap<i64, i64> = HashMap::new();
     for seed in &seeds {
-        tmp_map.insert(*seed, *seed);
+        seed_map.insert(*seed, *seed);
     }
-    let mut map_vec: Vec<HashMap<i64, i64>> = vec![tmp_map.clone()];
-    tmp_map.clear();
     
     let mut building: bool = false;
+    let mut tmp_map: HashMap<(i64, i64), (i64, i64)> = HashMap::new();
     for line in lines {
         if line.contains("map:") {
             println!("\n------\n{}", line);
             building = true;
             continue;
         }
-        
+
         if building && line.is_empty() {
-            println!("--Complete!");
             building = false;
-            map_vec.push(tmp_map.clone());
-            tmp_map.clear();
         }
         
         if building {
@@ -49,46 +44,51 @@ fn part1(input_lines: &String) -> i64 {
             let m2 = build_map(&mut map_input);
             tmp_map.extend(m2)
         }
-
-        if !building && map_vec.len() > 1 {
-            let mut pass_vec: Vec<HashMap<i64, i64>> = map_vec.clone();
-            map_vec.clear();
-            map_vec.push(compare_maps(pass_vec.pop().unwrap(), pass_vec.pop().unwrap()));
+        
+        if !building && !tmp_map.is_empty() {
+            seed_map = compare_maps(seed_map, tmp_map.clone());
+            tmp_map.clear();
         }
     }
-
-    let final_map: HashMap<i64, i64> = map_vec.pop().unwrap();
-    let mut result: i64 = *final_map.get(&seeds.pop().unwrap()).unwrap();
-    for seed in seeds.into_iter() {
-        let tmp = final_map.get(&seed).unwrap();
-        result = min(result, *tmp);
-    }
-    return result;
+    seed_map = compare_maps(seed_map, tmp_map.clone());
+    return *seed_map.values().min().unwrap();
 }
 
-
-fn build_map(nums: &mut Vec<i64>) -> HashMap<i64, i64> {
-    println!("--Building!");
+fn build_map(nums: &mut Vec<i64>) -> HashMap<(i64, i64), (i64, i64)> {
+    //print!("--Building!");
     if nums.len() < 2 { panic!() }
-    let range: i64 = nums.pop().unwrap();
-    let source: i64 = nums.pop().unwrap();
-    let destn: i64 = nums.pop().unwrap();
+    let range: i64 = nums[2];
+    let source: i64 = nums[1];
+    let destn: i64 = nums[0];
 
-    let map: HashMap<i64, i64> = (0..range).map(|x| (source+x, destn+x)).collect();
+    let mut map: HashMap<(i64, i64), (i64, i64)> = HashMap::new();
+    map.insert((source, source+range), (destn, destn+range));
+    //println!("{:?}", map);
     return map;
 }
 
-fn compare_maps(dest_map: HashMap<i64, i64>, source_map: HashMap<i64, i64>) -> HashMap<i64, i64> {
-    println!("--Comparing!");
+fn compare_maps(source_map: HashMap<i64, i64>, dest_map: HashMap<(i64, i64), (i64, i64)>) -> HashMap<i64, i64> {
+    //println!("--Comparing!");
+    //println!("src: {:?}", source_map);
+    //println!("dst {:?}", dest_map);
     let mut map: HashMap<i64, i64> = HashMap::new();
     for (seed, value) in &source_map {
-        if dest_map.contains_key(&value) {
-            let (key, val) = dest_map.get_key_value(&value).unwrap();
-            map.insert(*seed, *val);
-        } else {
+        let mut found: bool = false;
+        for ((source_min, source_max), (dest_min, _dest_max)) in &dest_map {
+            if value >= &source_min && value < &source_max {
+                //println!("sMin {}, dMin {}", source_min, dest_min);
+                let diff = dest_min-source_min ;
+                //println!("Found Seed[{}] {}-->{} ({})", seed, value, value+diff, diff);
+                map.insert(*seed, value+diff);
+                found = true;
+                break;
+            } 
+        }
+        if  !found {
             map.insert(*seed, *value);
         }
     }
+    //println!("{:?}", map);
     return map;
 }
 
