57c7a5df07da7267fedaa6470b4207767757ceac
bdstyerwalt <bdstyerwalt@gmail.com>
2023-12-06 23:56:25 -0800

    Solved Day 6 Part 2

--- day6/src/bin/part2.rs
+++ day6/src/bin/part2.rs
@@ -4,18 +4,35 @@ fn main() {
     dbg!(output);
 }
 
-fn process(input: &str) -> i32 {
-    todo!()
+fn process(input: &str) -> i64 {
+    let mut lines = input.lines();
+
+    let time: Vec<&str> = lines.nth(0).unwrap().split(":").collect();
+    let time: i64 = time[1].replace(" ", "").parse().unwrap();
+    println!("{:?}", time);
+
+    let distance: Vec<&str> = lines.nth(0).unwrap().split(":").collect();
+    let distance: i64 = distance[1].replace(" ", "").parse().unwrap();
+    println!("{:?}", distance);
+
+    let mut win_count: i64 = 0;
+    for t in 0..time {
+        let score = (time - t) * t;
+        if score > distance {
+            win_count += 1;
+        }
+    }
+
+    return win_count;
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
     use crate::process;
 
     #[test]
     fn part2_sample() {
-        let input_file: String = fs::read_to_string("sample.txt").expect("Couldn't read file");
-        assert_eq!(35, process(&input_file));
+        let input_file: &str = include_str!("sample.txt");
+        assert_eq!(71503, process(&input_file));
     }
 }
