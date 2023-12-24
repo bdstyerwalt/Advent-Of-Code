e123b2b33cb1bb5621477d805994795394544496
bdstyerwalt <bdstyerwalt@gmail.com>
2023-12-23 18:34:30 -0800

    Added Template Bin Folder

---
+++ template/part1.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn parse(input: &str) -> Puzzle { 
+
+}
+
+fn process(input: &str) -> usize {
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_sample() {
+        let input = include_str!("sample.txt");
+        assert_eq!(51, process(input));
+    }
+}
