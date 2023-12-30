aab774db8ca5f8f9200b69de4abd1d674ea852c2
bdstyerwalt <bdstyerwalt@gmail.com>
2023-12-30 14:01:49 -0800

    Updating Template

--- template/main.rs
+++ template/main.rs
@@ -1,16 +1,22 @@
 fn main() {
     let input = include_str!("input.txt");
-    let output = process(input);
-    dbg!(output);
+    let p1 = part1(input);
+    let p2 = part2(input);
+    println!("Part 1: {} | Part 2: {}", p1, p2);
 }
 
 fn parse(input: &str) -> Puzzle { 
     todo!();
 }
 
-fn process(input: &str) -> usize {
+fn part1(input: &str) -> u32 {
     todo!();
 }
+
+fn part2(input: &str) -> u32 {
+    todo!();
+}
+
 
 struct Puzzle {
     
