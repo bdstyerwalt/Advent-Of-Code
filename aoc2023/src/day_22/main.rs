0f0a4c05084fe065d172aae788f4fc7144bb352a
bdstyerwalt <bdstyerwalt@gmail.com>
2024-06-03 21:23:18 -0700

    Migrated Day 22

--- aoc2023/src/day_22/main.rs
+++ aoc2023/src/day_22/main.rs
@@ -1,7 +1,8 @@
 
 fn main() {
     let input = include_str!("input.txt");
-    let p1 = part1(input, 64);
-    let p2 = part2(input, 26501365);
+    let p1 = part1(input);
+    let p2 = part2(input);
     println!("Part 1: {} | Part 2: {}", p1, p2);
 }
+
