2ea4752fb2f15e3aefd14103a4517beef429df92
bdstyerwalt <bdstyerwalt@gmail.com>
2025-03-16 15:19:47 -0700

    Moved 2024 D7P1 to an IMPL function

--- aoc2024/src/day_07/mod.rs
+++ aoc2024/src/day_07/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::{collections::{HashMap, VecDeque}, u64};
 
 use itertools::Itertools;
 
@@ -16,6 +16,86 @@ pub fn run() {
 #[derive(Debug)]
 struct Puzzle {
     equations: HashMap<u64, Vec<u64>>,
+}
+
+impl Puzzle {
+    fn calibrate_equations(&self, operators: &Vec<String>) -> u64 {
+        let mut calibration_res = 0;
+        for (total, nums) in &self.equations {
+            let total = *total;
+            // println!("\nSearching for {total} ->");
+            
+            let mut deq: VecDeque<(u64, Vec<u64>, Vec<String>)> = VecDeque::new();
+            deq.push_front((nums[0].clone(), nums[1..].to_vec().clone(), vec![]));
+            // println!("{:?}", deq);
+            
+            while let Some((acc, num_vec, mut op_vec)) = deq.pop_front() {
+                // println!("{acc} __ {:?} __ {:?} ___ DEQ Size: {}", num_vec, op_vec, deq.len());
+    
+                if let None = num_vec.get(0) {
+                    // println!("RAN OUT OF NUMBERS...\n");
+                    break;
+                }
+    
+                let n = num_vec.get(0).unwrap();
+                let rem_num = num_vec[1..].to_vec();
+                // println!("REM: {:?}", rem_num);
+    
+                let mul = acc * n;
+                if mul == total  && rem_num.len() == 0 {
+                    op_vec.push("*".to_string());
+                    println!("{}", create_equation_string(total, nums.clone(), &op_vec));
+                    calibration_res += mul;
+                    break;
+                } else if mul < total {
+                    // clone so we don't affect the op string for the add branch 
+                    let mut mul_op_vec = op_vec.clone(); 
+                    mul_op_vec.push("*".to_string());
+                    deq.push_back((mul, rem_num.clone(), mul_op_vec));
+                }
+    
+                let add = acc + n;
+                if add == total && rem_num.len() == 0 {
+                    op_vec.push("+".to_string());
+                    println!("{}", create_equation_string(total, nums.clone(), &op_vec));
+                    calibration_res += add;
+                    break;
+                } else if add < total {
+                    op_vec.push("+".to_string());
+                    deq.push_back((add, rem_num, op_vec));
+                }
+            }
+        }
+        return calibration_res;
+    }
+}
+
+enum Operators {
+    ADD,
+    MULT,
+    CONCAT,
+}
+
+impl Operators {
+    fn get_string(&self) -> String {
+        match self {
+            Operators::ADD => return String::from("+"),
+            Operators::MULT => return String::from("*"),
+            Operators::CONCAT => return String::from("||"),
+        }
+    }
+
+    fn eval(&self, left: u64, right: u64) -> u64 {
+        match self {
+            Operators::ADD => return left + right,
+            Operators::MULT => return left * right,
+            Operators::CONCAT => {
+                let err_message = format!("Failed to parse {left}{right}");
+                let temp = format!("{left}{right}").parse().expect(err_message.as_str());
+                return temp;
+            }
+        }
+    }
 }
 
 fn parse(input: &str) -> Puzzle {
@@ -36,56 +116,8 @@ fn parse(input: &str) -> Puzzle {
 
 fn part1(input_file: &str) -> u64 {
     let puzzle = parse(input_file);
-    let mut calibration_res = 0;
+    let calibration_res = puzzle.calibrate_equations(&vec![]);
 
-    for (total, nums) in puzzle.equations {
-        // println!("\nSearching for {total} ->");
-        
-        let mut deq: VecDeque<(u64, Vec<u64>, Vec<String>)> = VecDeque::new();
-        deq.push_front((nums[0].clone(), nums[1..].to_vec().clone(), vec![]));
-        // println!("{:?}", deq);
-        
-        while let Some((acc, num_vec, mut op_vec)) = deq.pop_front() {
-            // println!("{acc} __ {:?} __ {:?} ___ DEQ Size: {}", num_vec, op_vec, deq.len());
-
-            if let None = num_vec.get(0) {
-                // println!("RAN OUT OF NUMBERS...\n");
-                break;
-            }
-
-            let n = num_vec.get(0).unwrap();
-            let rem_num = num_vec[1..].to_vec();
-            // println!("REM: {:?}", rem_num);
-
-            let mul = acc * n;
-            if mul == total  && rem_num.len() == 0 {
-                op_vec.push("*".to_string());
-                println!("{}", create_equation_string(total, nums.clone(), &op_vec));
-                calibration_res += mul;
-                break;
-            } else if mul < total {
-                // clone so we don't affect the op string for the add branch 
-                let mut mul_op_vec = op_vec.clone(); 
-                mul_op_vec.push("*".to_string());
-                deq.push_back((mul, rem_num.clone(), mul_op_vec));
-            }
-
-            let add = acc + n;
-            if add == total && rem_num.len() == 0 {
-                op_vec.push("+".to_string());
-                println!("{}", create_equation_string(total, nums.clone(), &op_vec));
-                calibration_res += add;
-                break;
-            } else if add < total {
-                op_vec.push("+".to_string());
-                deq.push_back((add, rem_num, op_vec));
-            }
-        }
-
-
-
-
-    }
     return calibration_res;
 }
 
